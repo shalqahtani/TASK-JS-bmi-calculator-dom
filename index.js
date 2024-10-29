1c0cc51e5251f5a922c00689fe15facc077cf2ca
unknown <eng.shahad.alqahtani@gmail.com>
2024-10-29 18:53:15 +0300

    BMI

--- index.js
+++ index.js
@@ -8,4 +8,20 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+  let heightInM= height*10;
+  let BMI = heightInM * weight;
+  
+  if (BMI <18.5){
+    alert("Under Weight");
+  }
+  if(BMI >= 18.5 && BMI <= 24.9){
+   alert("Healthy Weight");
+  }
+  if(BMI >=25.0 && BMI <=29.9){
+   alert("Over Weight");
+  }
+   if(BMI>=30.0){
+    alert ("Obesity");
+   }
 }
+
